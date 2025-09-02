7dcf1fa05c785f809fcb9cab10bb697d8522ae75
Premkmj <bishalkmjha@gmail.com>
2025-09-02 19:40:10 +0530

    app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,5 @@
 async function registerUser(username, password) {
-  const res = await fetch("http://127.0.0.1:8000/register", {
+  const res = await fetch("https://hostelmate-backend.onrender.com/register", { ... })
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({ username, password })
